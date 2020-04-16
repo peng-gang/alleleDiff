5bd3cb80a2f02300979c4941ce5b9889e090eeed
gpeng <michael.gang.peng@gmail.com>
2020-04-15 22:02:07 -0400

    Initial Commit

---
+++ alleleDiff/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  alleleDiff
+//
+//  Created by Gang Peng on 4/15/20.
+//  Copyright © 2020 Gang Peng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
